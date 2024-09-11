248cfaf63177320eb968d377c9b957748c4d1ed2
amazing79 <ignacioja@hotmail.com>
2024-09-10 21:17:51 -0300

    Ajustes a composer.json. Se agrego nombre proyecto, version de php requerida

--- backend/public/index.php
+++ backend/public/index.php
@@ -1,9 +1,17 @@
 <?php
+declare(strict_types=1);
+
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
-require __DIR__ . '/../vendor/autoload.php';
+define ('APP_ROOT', dirname(__DIR__));
+
+require APP_ROOT . '/vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(APP_ROOT);
+$dotenv->load();
+
 
 $app = AppFactory::create();
 
