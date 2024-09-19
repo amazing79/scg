289f507bff2fa363dd479dd4bb43eec31fee2342
amazing79 <ignacioja@hotmail.com>
2024-09-19 14:54:44 -0300

    refactoring de index para mostrar la lista de categorias en una tabla

--- frontend/assets/js/categorias/categorias.js
+++ frontend/assets/js/categorias/categorias.js
@@ -1,37 +1,52 @@
 import {config} from "../config.js";
 import {actions} from "./actions.js";
 
-async function getCategorias()
+const closeButton = document.getElementById("cancelBtn");
+const confirmBtn = document.getElementById("confirmBtn");
+
+async function getCategories()
 {
     let request = new URL(actions.PATH ,config.URL_API);
     let data = await fetch(request);
     return await data.json();
 }
 
-export {getCategorias}
-
-function loadCategorias()
+function loadCategories()
 {
-    getCategorias()
+    getCategories()
         .then(result => {
-           let lista = document.getElementById('categorias');
+           let lvw = document.getElementById('lvw-data');
            let categorias = result.data;
-           let html = '<ul>';
 
-           categorias.forEach( (categoria) => {
-               html += `<li>${categoria['descripcion']}</li>`
-            });
-            html += "</ul>";
-            lista.innerHTML = html;
-            })
+           categorias.forEach((categoria) => {
+               let row = loadDataRow(categoria, 'idCategoria');
+               lvw.appendChild(row);
+           });
+        })
         .catch( error => {
             console.log('Error al obtener las categorias')
         });
 }
 
+closeButton.addEventListener("click", () => {
+    let page = document.getElementById('body');
+    const dialog = document.getElementById("dialog_confirm");
+    page.classList.remove('bg__blur');
+    dialog.close();
+});
+confirmBtn.addEventListener("click", (event) => {
+    event.preventDefault();
+    let page = document.getElementById('body');
+    const dialog = document.getElementById("dialog_confirm");
+    evt_delete(localStorage.getItem('idRow'));
+    page.classList.remove('bg__blur');
+    dialog.close();
+    localStorage.clear();
+});
+
 function init()
 {
-    loadCategorias();
+    loadCategories();
 }
 
 window["onload"] = init;
