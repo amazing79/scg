72bd1a7f27409e6915cb804a7c7cbb57b291edd1
amazing79 <ignacioja@hotmail.com>
2024-10-19 22:19:08 -0300

    Modulo Gastos: Trabajo en el frontend
    Se empezo a vincular el BE con FE.
    Se puede agregar, modificar y eliminar gastos.
    Queda refactorizar comunicacion de los datos enviados por el BE para mas acordes a lo que se quiere mostrar en el FE.
    Faltan validaciones de que no se agreguen gastos si no existe la categoria seleccionada ni la persona

---
+++ frontend/assets/js/gastos/actions.js
@@ -0,0 +1,15 @@
+const PATH = "gastos";
+const LIST = "GET";
+const SHOW = "GET";
+const EDIT = "PATCH";
+const DELETE = "DELETE";
+const CREATE = 'POST';
+
+export const actions = {
+    PATH,
+    LIST,
+    SHOW,
+    EDIT,
+    DELETE,
+    CREATE
+}
