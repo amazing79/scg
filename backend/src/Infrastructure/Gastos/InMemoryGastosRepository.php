d18d285cae00c26735da19370d972d37b1610313
amazing79 <ignacioja@hotmail.com>
2024-10-31 11:15:34 -0300

    Tests: Se agrego test faltante de Personas. Se empezo a trabajar en Gastos

---
+++ backend/src/Infrastructure/Gastos/InMemoryGastosRepository.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Infrastructure\Gastos;
+
+use App\Domain\Gastos\Gastos;
+use App\Domain\Gastos\GastosRepository;
+use App\Infrastructure\Common\MemoryDB;
+
+class InMemoryGastosRepository implements GastosRepository
+{
+
+    public function __construct(private ?MemoryDB $db = null)
+    {
+        $this->db = $this->db ?? new MemoryDB();
+    }
+
+    public function create($data): int
+    {
+        return $this->db->add($data);
+    }
+
+    public function update($data): void
+    {
+        /**
+         * @var Gastos $data
+         */
+        $this->db->set($data->getIdGasto(), $data);
+    }
+
+    public function delete($id): void
+    {
+        $this->db->remove($id);
+    }
+
+    public function findById($id): ?Gastos
+    {
+        return $this->db->get($id);
+    }
+
+    public function getAll(): array
+    {
+        return $this->db->getAll();
+    }
+
+    public function find($filter): array
+    {
+        return $this->db->find($filter, 'getDescripcion');
+    }
+}
